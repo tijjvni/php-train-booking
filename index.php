13902b0702dda68098cdd28c8706be41bc95a5ca
tijjvni <tijjvni@gmail.com>
2021-09-01 16:54:37 +0000

    update

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
     
     $pdo = require_once 'connect.php';
     
-    require_once('functions.php');
+    // require_once('functions.php');
 
     // $user = authenticate();
-
+?>
+    <a href="logout">logout</a>
+<?php
     print_r($_SESSION['user']);
--- logout.php
+++ logout.php
@@ -3,3 +3,5 @@
     $pdo = require_once 'connect.php';
     
     session_destroy();
+
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
