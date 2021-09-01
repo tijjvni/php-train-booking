9627e2ea2dbff3277b9d8b8399c846d406b37739
tijjvni <tijjvni@gmail.com>
2021-09-01 09:02:09 +0000

    Revert "adminer added"
    This reverts commit ea647d10823582536778497ba899e2e565f360ee.

--- config.php
+++ config.php
@@ -1,13 +1,6 @@
 <?php
 
-// $host     = 'host';
-// $db       = 'db';
-// $user     = 'user';
-// $password = 'password';
-
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$host = $url["host"];
-$user = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$host     = 'us-cdbr-east-04.cleardb.com';
+$db       = 'heroku_0ccfdd994b7c83e';
+$user     = 'ba9050f09beeaf';
+$password = '48a44fea';
