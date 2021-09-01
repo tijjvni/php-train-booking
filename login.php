6a9a64bd09db2085980907dc4ceba2775b58e0fa
tijjvni <tijjvni@gmail.com>
2021-09-01 11:17:14 +0000

    authentication added

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+    $pdo = require 'connect.php';
+
+    // login user
+    if( isset($_POST['submit']) && !empty($_POST['submit']) ){
+        
+        extract($_POST);
+
+        // check if there's an empty field
+        if(empty($email) || empty($password)){
+            echo "All fields are required";
+        }else { 
+
+
+            $sql = 'SELECT * 
+            FROM users
+            WHERE email = :email
+            AND password = :password';
+    
+            $statement = $pdo->prepare($sql);
+            $statement->bindParam(':email', $email, PDO::PARAM_INT);
+            $statement->bindParam(':password', sha1($password), PDO::PARAM_INT);
+            $statement->execute();
+            $user = $statement->fetch(PDO::FETCH_ASSOC);
+            
+            if ($user) {
+                $_SESSION['user'] = $email;
+                header('Location: index.php');
+            } else {
+                echo "user not found.";
+            }            
+
+
+        }
+
+    }
---
+++ register.php
@@ -0,0 +1,44 @@
+<?php
+    $pdo = require 'connect.php';
+
+    // check form submission
+    if( isset($_POST['submit']) && !empty($_POST['submit']) ){
+        
+        extract($_POST);
+
+        // check if there's an empty field
+        if(empty($first_name) || empty($last_name) || empty($email) || empty($password)){
+            echo "All fields are required";
+        }else {
+
+
+            // registeruser
+            $sql = 'INSERT INTO users(first_name, last_name, email, password) VALUES(:first_name, :last_name, :email, :password)';
+
+            $statement = $pdo->prepare($sql);
+
+            $statement->execute([
+                ':first_name' => $first_name,
+                ':last_name' => $last_name,
+                ':email' => $email,
+                ':password' => sha1($password)
+            ]);
+
+
+            echo 'Account created successfully. please <a href="login.php">login</a>';            
+
+
+        }
+
+    }
+
+?>
+<form action="post">
+    <input type="text" placeholder="first name" name="first_name"/>
+    <input type="text" placeholder="last name" name="first_name"/>
+    <input type="email" placeholder="email" name="email"/>
+    <input type="password" placeholder="password" name="password"/>
+
+    <input type="submit" value="submit" name="submit"/>
+    
+</form>
